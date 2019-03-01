b2b71bb19e306d9d3cebc34186a8da6c42008d31
陈全 <ralfbawg@gmail.com>
2019-03-01 11:30:30 +0800

    初始导入

--- src/config/config.go
+++ src/config/config.go
@@ -9,22 +9,22 @@ import (
 type configure struct {
 	common struct{
 		env string
-	}
+	}`yaml:"common"`
 	log struct{
 		filepath string
-	}
+	}`yaml:"log"`
 	wserver struct{
 		port int
-	}
+	}`yaml:"wsserver"`
 	apiserver struct{
 		port int
-	}
+	}`yaml:"apiserver"`
 	db     struct {
-		url      string `json"url"`
-		username string `json"username"`
-		password string `json"password"`
-		dbtype   string `json"dbtype"`
-	} `json:"db"`
+		url      string `yaml"url"`
+		username string `yaml"username"`
+		password string `yaml"password"`
+		dbtype   string `yaml"dbtype"`
+	} `yaml:"db"`
 }
 
 func (c *configure) getConfig() (*configure, error) {
